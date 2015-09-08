a986401fe8946e4f8874732072c6aeaa2ece6e93
JuanJo <JuanJo@JuanJo-PC>
2015-09-08 09:53:12 -0500

    Juan: Esta modificando archivos

--- src/clientews/ClienteWS.java
+++ src/clientews/ClienteWS.java
@@ -14,6 +14,6 @@ public class ClienteWS {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here // Esta actualizando en GitHub jjaj
+        // TODO code application logic here // Esta actualizando en GitHub jj
     }
 }
